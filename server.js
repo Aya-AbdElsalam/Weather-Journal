707cb8dda39c3bc75c99786d1dc86a9bf1c4624c
Aya-AbdElsalam <ayam28040@gmail.com>
2024-09-28 20:52:44 +0300

    first commit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use(express.static("website"));
+app.use(express.static("public"));
 
 // Setup Server
 const port = 3000;
